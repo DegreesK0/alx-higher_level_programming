1daea064045d7c0cb77e8a20200d1880d3cb7f82
DegreesK0 <kelvinbora@gmail.com>
2023-12-19 17:43:19 +0000

    Upload

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """Square Class"""
+    def __init__(self, size):
+        """Square Initialization
+        Args: size - represents the size of the square defined
+        """
+
+        self.__size = size
