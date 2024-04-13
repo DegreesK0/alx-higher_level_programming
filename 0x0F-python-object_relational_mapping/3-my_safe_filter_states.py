346ad62df9fe5104fa9cc18ed62ef15d21830110
DegreesK0 <kelvinbora@gmail.com>
2024-04-13 11:18:05 +0000

    Upload

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -18,8 +18,11 @@ if __name__ == '__main__':
 
     db_cursor = db_connect.cursor()
 
-    db_cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' \
-            ORDER BY states.id ASC".format(argv[4]))
+    # db_cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' \
+    #         ORDER BY states.id ASC".format(argv[4]))
+
+    db_cursor.execute("SELECT * FROM states WHERE name LIKE BINARY %(name)s \
+            ORDER BY states.id ASC", {'name': argv[4]})
 
     rows_selected = db_cursor.fetchall()
 
