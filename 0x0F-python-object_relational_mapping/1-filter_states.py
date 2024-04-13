e4a4c637b24ec0419828b098b40fbbc0d4d97d2f
DegreesK0 <kelvinbora@gmail.com>
2024-04-13 11:02:06 +0000

    Upload

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,20 +6,43 @@ in ascending order by 'states.id'
 from the database hbtn_0e_0_usa
 """
 
-import MySQLdb
+# import MySQLdb
+# from sys import argv
+
+# if __name__ == '__main__':
+#     """
+#     Access the database for the states
+#     """
+#     db_connect = MySQLdb.connect(
+#         host="localhost", user=argv[1], port=3306, passwd=argv[2],
+# db=argv[3])
+
+#     db_cursor = db_connect.cursor()
+
+#     db_cursor.execute("SELECT * FROM states WHERE name \
+#             LIKE 'N%' ORDER BY states.id ASC")
+
+#     rows_selected = db_cursor.fetchall()
+
+#     for row in rows_selected:
+#         print(row)
+
+import MySQLdb as db
 from sys import argv
 
-if __name__ == '__main__':
-    """
-    Access the database for the states
-    """
-    db_connect = MySQLdb.connect(
-        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+"""
+Access to the database and get the states
+from the database.
+"""
 
+if __name__ == '__main__':
+    db_connect = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
     db_cursor = db_connect.cursor()
 
-    db_cursor.execute("SELECT * FROM states WHERE name \
-            LIKE 'N%' ORDER BY states.id ASC")
+    db_cursor.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' \
+                ORDER BY states.id ASC")
 
     rows_selected = db_cursor.fetchall()
 
